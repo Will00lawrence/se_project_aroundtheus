bafef8985c660c4bfa356f142357b4a50800a810
will00lawrence <will00lawrence@gmail.com>
2024-03-31 14:41:46 -0500

    project 4 part 1 second submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const intialCards = [
+const initialCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
